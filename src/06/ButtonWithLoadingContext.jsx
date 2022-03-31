9d9180d25058fb1a66096fb1aaca0c49231d700d
ParkSoHyeon <dso753@naver.com>
2022-04-01 02:16:07 +0900

    Button issue - children is required

--- src/06/ButtonWithLoadingContext.jsx
+++ src/06/ButtonWithLoadingContext.jsx
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import Button from "../04/Button";
 import withLoadingContext from  './withLoadingContext';
 
-function ButtonWithLoadingContext({ label, loading, setLoading }) {
+function ButtonWithLoadingContext({ children, loading, setLoading }) {
     return (
         <Button onPress={() => setLoading(!loading)}>
-            {loading ? '로딩 중' : label}
+            {loading ? '로딩 중' : children}
         </Button>
     )
 }
