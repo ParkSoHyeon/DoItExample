7cf0fa5b01578d5d366e387dd014e36cae31e5dc
ParkSoHyeon <dso753@naver.com>
2022-03-31 16:08:10 +0900

    multiple higher order component sample

--- src/stories/LifecycleStory.jsx
+++ src/stories/LifecycleStory.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from "@storybook/react";
 
-import { PageWithLoadData } from "../05/lifecycle";
+import {PageWithLoadData, PageWithLoadDataAndLoading} from "../05/lifecycle";
 
 storiesOf('Lifecycle', module)
     .addWithJSX('loadData 예제', () => (
         <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />
+    ))
+    .addWithJSX('loading Message 예제', () => (
+        <PageWithLoadDataAndLoading loadData={() => fetch('/').then(() => 'hello')} />
     ))
