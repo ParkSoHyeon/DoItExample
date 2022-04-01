83d1983b071b9f5dfa575dfdc8404603588fb6b1
ParkSoHyeon <dso753@naver.com>
2022-04-01 17:09:56 +0900

    ModalProvider - 소비자가 연결된 Button 컴포넌트

--- src/stories/ModalStory.jsx
+++ src/stories/ModalStory.jsx
@@ -5,6 +5,7 @@ import Modal from "../06/Modal";
 import Button from "../04/Button";
 import Text from "../04/Text";
 import ButtonWithModal from "../06/ButtonWithModal";
+import ModalProvider, { Consumer } from "../06/ModalProvider";
 
 storiesOf('Modal', module)
     .addWithJSX('기본 설정', () => (
@@ -14,4 +15,14 @@ storiesOf('Modal', module)
             <Button>닫기</Button>
         </Modal>
     ))
-.addWithJSX('ButtonWithModal', () => <ButtonWithModal />)
+    .addWithJSX('ButtonWithModal', () => <ButtonWithModal />)
+    .addWithJSX('ModalProvider', () => (
+        <ModalProvider>
+            <div>
+                <Text>다음 버튼 눌러 모달을 실행합니다.</Text>
+                <Consumer>
+                    {({ openModal }) => <Button onPress={() => openModal()}>삭제</Button>}
+                </Consumer>
+            </div>
+        </ModalProvider>
+    ))
