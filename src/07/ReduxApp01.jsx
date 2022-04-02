62129ac340abde80e86d25175040f24cb6a76765
ParkSoHyeon <dso753@naver.com>
2022-04-02 19:22:25 +0900

    reducer 구현

--- src/07/ReduxApp01.jsx
+++ src/07/ReduxApp01.jsx
@@ -2,9 +2,22 @@ import React, {PureComponent} from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+const reducer = (state, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case 'SET_LOADING': {
+            return {
+                ...state,
+                loading: payload
+            };
+        }
+        default: return state
+    }
+}
+
 class ReduxApp01 extends PureComponent {
     store = createStore(
-        state => state,
+        reducer,
         { loading: false, name: '두잇 리액트' },
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
