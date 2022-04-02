17e5ac230df766e8f466c92761889f66e753ebe2
ParkSoHyeon <dso753@naver.com>
2022-04-02 20:00:36 +0900

    user 액션 분리에 따른 코드 수정

--- src/07/ReduxApp01.jsx
+++ src/07/ReduxApp01.jsx
@@ -2,16 +2,14 @@ import React, {PureComponent} from 'react';
 import { Provider } from 'react-redux';
 import configureStore from "./configureStore";
 import { setLoading, resetLoading } from "./actions/loadingActions";
+import { setUser } from "./actions/userActions";
 
 class ReduxApp01 extends PureComponent {
     store = configureStore({ loading: false });
     componentDidMount() {
         this.store.dispatch(setLoading(true));
         this.store.dispatch(resetLoading());
-        this.store.dispatch({
-            type: 'SET_USER',
-            payload: { name: 'Park', age: 27 }
-        });
+        this.store.dispatch(setUser({ name: 'Park', age: 27 }));
     }
     render() {
         return (
