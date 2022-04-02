34b38cba448b9a48204925c9cb0772a1d6a68bb1
ParkSoHyeon <dso753@naver.com>
2022-04-02 20:43:40 +0900

    collection reducer 실행 확인

--- src/07/ReduxApp01.jsx
+++ src/07/ReduxApp01.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import configureStore from "./configureStore";
 import { setLoading, resetLoading } from "./actions/loadingActions";
 import { setUser } from "./actions/userActions";
+import { setCollection } from "./actions/collectionActions";
 
 class ReduxApp01 extends PureComponent {
     store = configureStore({ loading: false });
@@ -10,6 +11,10 @@ class ReduxApp01 extends PureComponent {
         this.store.dispatch(setLoading(true));
         this.store.dispatch(resetLoading());
         this.store.dispatch(setUser({ name: 'Park', age: 27 }));
+        this.store.dispatch(setCollection([
+            { id: 1, name: 'Park', age: 27 },
+            { id: 2, name: 'Choi', age: 20 },
+        ]));
     }
     render() {
         return (
