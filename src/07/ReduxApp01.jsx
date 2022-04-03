e15a543f8bca8d4b336e297d1a291891bc3e4fdd
ParkSoHyeon <dso753@naver.com>
2022-04-04 01:05:17 +0900

    collection 분리

--- src/07/ReduxApp01.jsx
+++ src/07/ReduxApp01.jsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import configureStore from "./configureStore";
 import { setLoading, resetLoading } from "./actions/loadingActions";
 import { setUser } from "./actions/userActions";
-import {setAge, setCollection} from "./actions/collectionActions";
 
 class ReduxApp01 extends PureComponent {
     store = configureStore({ loading: false });
@@ -11,15 +10,6 @@ class ReduxApp01 extends PureComponent {
         this.store.dispatch(setLoading(true));
         this.store.dispatch(resetLoading());
         this.store.dispatch(setUser({ name: 'Park', age: 27 }));
-        this.store.dispatch(setCollection([
-            { id: 1, name: 'Park', age: 27 },
-            { id: 2, name: 'Choi', age: 20 },
-        ]));
-        this.store.dispatch(setAge(2, 35));
-        const { collection } = this.store.getState();
-        const { ids, entities } = collection;
-        const originalPayload = ids.map(id => entities[id]);
-        console.log(originalPayload);
     }
     render() {
         return (
