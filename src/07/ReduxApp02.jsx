98c23bb92ada1168abb17343b12446c1e9e02529
ParkSoHyeon <dso753@naver.com>
2022-04-04 01:06:32 +0900

    데이터 컴포넌트 출력 및 실행 확인

---
+++ src/07/ReduxApp02.jsx
@@ -0,0 +1,32 @@
+import React, {PureComponent} from 'react';
+import { Provider } from 'react-redux';
+import configureStore from "./configureStore";
+import {setAge, setCollection} from "./actions/collectionActions";
+import PresentationComponent from "./PresentationComponent";
+import ContainerComponent from "./containers/ContainerComponent";
+
+class ReduxApp02 extends PureComponent {
+    store = configureStore({ loading: false });
+    componentDidMount() {
+        this.store.dispatch(setCollection([
+            { id: 1, name: 'Park', age: 27 },
+            { id: 2, name: 'Choi', age: 20 },
+        ]));
+        this.store.dispatch(setAge(2, 35));
+        const { collection } = this.store.getState();
+        const { ids, entities } = collection;
+        const originalPayload = ids.map(id => entities[id]);
+        console.log(originalPayload);
+    }
+    render() {
+        return (
+            <Provider store={this.store}>
+                화면 컴포넌트: <PresentationComponent userName="화면 컴포넌트" />
+                <br />
+                데이터 컴포넌트: <ContainerComponent id={2} />
+            </Provider>
+        );
+    }
+}
+
+export default ReduxApp02;
--- src/stories/ReduxAppStory.jsx
+++ src/stories/ReduxAppStory.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from "@storybook/react";
 import ReduxApp01 from "../07/ReduxApp01";
+import ReduxApp02 from "../07/ReduxApp02";
 
 storiesOf('ReduxApp', module)
     .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
+    .addWithJSX('데이터 컴포넌트 예제', () => <ReduxApp02 />)
