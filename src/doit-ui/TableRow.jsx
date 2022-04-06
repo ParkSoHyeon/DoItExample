01319d534174b54d48ffedc7fd100d6ce1e992cd
ParkSoHyeon <dso753@naver.com>
2022-04-06 17:05:30 +0900

    데이터 테이블 컴포넌트 구현 및 실행 확인

---
+++ src/doit-ui/TableRow.jsx
@@ -0,0 +1,19 @@
+import React, {PureComponent} from 'react';
+import PropTypes from "prop-types";
+
+class TableRow extends PureComponent {
+    render() {
+        const { children, isHeader, baseline } = this.props;
+        return (
+            <tr>
+                {React.Children.map(children, child => React.cloneElement(child, { baseline, isHeader }))}
+            </tr>
+        );
+    }
+}
+
+TableRow.propTypes = {
+    children: PropTypes.node,
+}
+
+export default TableRow;
