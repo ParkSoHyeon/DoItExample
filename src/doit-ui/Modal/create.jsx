7031c0681daa51e3da752c42243a0e9c0b026656
ParkSoHyeon <dso753@naver.com>
2022-04-06 17:30:33 +0900

    Modal 컴포넌트(provider) 구현 및 실행 확인

---
+++ src/doit-ui/Modal/create.jsx
@@ -0,0 +1,43 @@
+import React, {PureComponent} from 'react';
+import Modal from "../Modal";
+
+const { Provider } = React.createContext({});
+
+export default function createModalProvider(ContentMap = {}) {
+    return class ModalProvider extends PureComponent {
+        constructor(props) {
+            super(props);
+
+            this.state = { showModal: false };
+            this.handleClose = this.handleClose.bind(this);
+            this.handleOpen = this.handleOpen.bind(this);
+        }
+        handleClose() {
+            this.setState({ showModal: false })
+        }
+        handleOpen(contentId, modalProps) {
+            this.contentId = contentId;
+            this.modalProps = modalProps;
+            this.setState({ showModal: true });
+        }
+        render() {
+            const { children } = this.props;
+            const { showModal } = this.state;
+            const ModalContent = ContentMap[this.contentId];
+
+            return (
+                <Provider value={{
+                    openModal: this.handleOpen,
+                    closeModal: this.handleClose
+                }}>
+                    {children}
+                    {showModal && ModalContent && (
+                        <Modal>
+                            <ModalContent {...this.modalProps} />
+                        </Modal>
+                    )}
+                </Provider>
+            );
+        }
+    }
+};
