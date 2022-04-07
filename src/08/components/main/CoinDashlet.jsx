53bc96b1b2b952b51d6ef1a41a049b1eec1eadce
ParkSoHyeon <dso753@naver.com>
2022-04-07 17:34:48 +0900

    매도/매수 버튼에 모달 연결

--- src/08/components/main/CoinDashlet.jsx
+++ src/08/components/main/CoinDashlet.jsx
@@ -5,21 +5,37 @@ import Card from "../../../doit-ui/Card";
 import InlineList from "../../../doit-ui/InlineList";
 import Text from "../../../doit-ui/Text";
 import PropTypes from "prop-types";
+import Modal from "../../../doit-ui/Modal";
+import {TRADE_COIN_MODAL} from "../../constants/modals";
 
 class CoinDashlet extends PureComponent {
     render() {
         const { name, priceLabel } = this.props;
         return (
-            <Card vertical={4} horizontal={4}>
-                <Heading level={4}>
-                    {name}
-                    <Text>{priceLabel}</Text>
-                </Heading>
-                <InlineList spacingBetween={1}>
-                    <Button primary small>매도</Button>
-                    <Button small>매수</Button>
-                </InlineList>
-            </Card>
+            <Modal>
+                {({ openModal }) => (
+                    <Card vertical={4} horizontal={4}>
+                        <Heading level={4}>
+                            {name}
+                            <Text>{priceLabel}</Text>
+                        </Heading>
+                        <InlineList spacingBetween={1}>
+                            <Button
+                                primary small
+                                onPress={() => openModal(TRADE_COIN_MODAL, { type: 'buy', name, price: priceLabel })}
+                            >
+                                매도
+                            </Button>
+                            <Button
+                                small
+                                onPress={() => openModal(TRADE_COIN_MODAL, { type: 'sell', name, price: priceLabel })}
+                            >
+                                매수
+                            </Button>
+                        </InlineList>
+                    </Card>
+                )}
+            </Modal>
         );
     }
 }
